3fa1c06d68faccc7a2e3121a0402938e8614694c
Miguel Sánchez <a324795@uach.mx>
2023-05-15 01:31:34 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# PROYECTO DEL SEGUNDO PARCIAL
+
+**En este proyecto crearemos nuestro primer sitio web**
+
+- *Miguel Eduardo Sánchez González (324795)*
+- *Jose Ricardo Avila Chávez (345635)*
+- *Sebastian Meza Andrade (339079)*
+
+## Links de los repositorios
+Github:
+
+Dockerhub:
+
+## Pre-requisitos
+
+Descargar el archivo <sub>index.html</sub> y la carpeta <sub>www</sub> con todo su contenido
+
+## Correr archivo
+
+Podemos correr el servidor de esta manera:
+```
+node automax.js
+```
+Abrimos en un navegador http://localhost:8888/ para ver el Landing page de la empresa.
+
+Tambien se puede abrir la pagina http://localhost:8888/contactos para ver los datos para ubicar a la empresa.
+
+Se puede encontrar los datos de la empresa:
+
++ Misión
++ Visión
+
+En la siguiente pagina: http://localhost:8888/acerca
+
+En la pagina principal, o Landing page, podemos encontrar los links directos que nos llevaran a los contactos o informacion de la empresa, asi como algunos de los vehiculos mas vendidos, los cuales nos direccionaran a los detalles o especificaciones de cada uno de estos.
---
+++ automax.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const fs = require('fs');
+
+// HTTP => (request, response)
+
+http.createServer((request, response)=>{
+
+  const file = request.url == '/' ?
+  './www/index.html' : `./www${request.url}`;
+
+  // const data = fs.readFileSync('./www/index.html');
+  fs.readFile(file, (err, data) =>{
+    if(err){
+      response.writeHead(404, {"Content-Type":"text/plain"});
+      response.write("Not found");
+      response.end();
+    }else{
+      const extension = request.url.split('.').pop();
+      console.log(extension);
+      switch (extension){
+        case 'txt':
+          response.writeHead(200, {"Content-Type":"text/plain"});
+          break;
+        case 'html':
+          response.writeHead(200, {"Content-Type":"text/html"});
+          break;
+        case 'css':
+          response.writeHead(200, {"Content-Type":"text/css"});
+          break;
+        case 'ico':
+          response.writeHead(200, {"Content-Type":"image/x-icon"});
+          break;
+        case 'js':
+          response.writeHead(200, {"Content-Type":"text/javascript"});
+          break;
+        case 'jpeg':
+          response.writeHead(200, {"Content-Type":"image/jpeg"});
+          break;
+        case 'png':
+          response.writeHead(200, {"Content-Type":"image/png"});
+          break;
+      default:
+        response.writeHead(200, {"Content-Type":"text/html"});
+      }
+      response.write(data);
+      response.end();
+    }
+  });
+}).listen(8888);
